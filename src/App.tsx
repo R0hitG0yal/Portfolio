6b866715924809dca2a428a88d4b650e03bab8a5
R0hitG0yal <rgoyal4122@gmail.com>
2024-07-02 10:09:51 +0530

    Added active state to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Intro from "./components/intro";
 import Navbar from "./components/navbar";
 
+
 function App() {
   return (
     <div className="bg-slate-900 text-slate-400 h-screen">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,33 @@
-const Navbar = () =>{
-    return (
-      <div className="flex justify-between px-32 py-2 font-serif">
-        <div className="text-3xl underline underline-offset-2">Rohit Goyal</div>
-        <div>
-          <ul className="flex items-center text-lg py-2">
-            <li className="min-w-120px px-4">
-              <a href="#intro">Who Am I?</a>
+import { useState } from "react";
+
+const Navbar = () => {
+  const [active, setActive] = useState<number>();
+  const list = [
+    { id: 1, text: "Who am I?" },
+    { id: 2, text: "Experience" },
+    { id: 3, text: "Education" },
+    { id: 4, text: "Skills" },
+    { id: 5, text: "Hire ME" },
+  ];
+  return (
+    <div className="flex justify-between px-32 py-2 font-serif">
+      <div className="text-3xl underline underline-offset-2">Rohit Goyal</div>
+      <div>
+        <ul className="flex items-center text-lg py-2 ">
+          {list.map((element) => (
+            <li
+              className={active === element.id ? "underline mx-4" : "mx-4"}
+              onClick={() => {
+                setActive(element.id);
+              }}
+            >
+              <a href="#intro">{element.text}</a>
             </li>
-            <li className="min-w-120px px-3">
-              <a href="#experience">Experience</a>
-            </li>
-            <li className="min-w-120px px-3">
-              <a href="#education">Education</a>
-            </li>
-            <li className="min-w-120px px-3">
-              <a href="#skills">Skills</a>
-            </li>
-            <li className="min-w-120px px-3">
-              <a href="#contact">Hire Me</a>
-            </li>
-          </ul>
-        </div>
+          ))}
+        </ul>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Navbar;
